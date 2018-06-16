73b61612e7905e5638b67229c3ff06e5d5e5ee81
Gonzalo <Gonzalo@192.168.0.19>
2018-06-16 10:08:08 +0200

    Commit para subir

---
+++ src/libreriagit_cod/LibreriaGit_COD.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package libreriagit_cod;
+
+/**
+ *
+ * @author Gonzalo
+ */
+public class LibreriaGit_COD {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Menu obx = new Menu();
+        obx.escogerMenu();
+        
+    }
+    
+}
